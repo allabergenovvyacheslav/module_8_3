148f4e6c6ad1cf7cee02107cf8e78e86f22b1161
Vyacheslav <slawushkin@mail.ru>
2024-08-12 17:55:45 +0300

    Update module_8_3.py

--- module_8_3.py
+++ module_8_3.py
@@ -1,13 +1,45 @@
+# Задача "Некорректность":
+#
+# Создайте 3 класса (2 из которых будут исключениями):
+# Класс Car должен обладать следующими свойствами:
+# Атрибут объекта model - название автомобиля (строка).
+# Атрибут объекта __vin - vin номер автомобиля (целое число). Уровень доступа private.
+# Метод __is_valid_vin(vin_number) - принимает vin_number и проверяет его на корректность.
+# Возвращает True, если корректный, в других случаях выбрасывает исключение. Уровень доступа private.
+# Атрибут __numbers - номера автомобиля (строка).
+# Метод __is_valid_numbers(numbers) - принимает numbers и проверяет его на корректность.
+# Возвращает True, если корректный, в других случаях выбрасывает исключение. Уровень доступа private.
+# Классы исключений IncorrectVinNumber и IncorrectCarNumbers, объекты которые обладают атрибутом
+# message - сообщение, которое будет выводиться при выбрасывании исключения.
+#
+# Работа методов __is_valid_vin и __is_valid_numbers:
+# __is_valid_vin
+# Выбрасывает исключение IncorrectVinNumber с сообщением 'Некорректный тип vin номер', если передано не
+# # целое число. (тип данных можно проверить функцией isinstance).
+# Выбрасывает исключение IncorrectVinNumber с сообщением 'Неверный диапазон для vin номера',
+# если переданное число находится не в диапазоне от 1000000 до 9999999 включительно.
+# Возвращает True, если исключения не были выброшены.
+# __is_valid_numbers
+# Выбрасывает исключение IncorrectCarNumbers с сообщением 'Некорректный тип данных для номеров',
+# если передана не строка. (тип данных можно проверить функцией isinstance).
+# Выбрасывает исключение IncorrectCarNumbers с сообщением 'Неверная длина номера', переданная строка
+# должна состоять ровно из 6 символов.
+# Возвращает True, если исключения не были выброшены.
+#
+# ВАЖНО!
+# Методы __is_valid_vin и __is_valid_numbers должны вызываться и при создании объекта (в __init__ при
+# объявлении атрибутов __vin и __numbers).
+
 class Car:
     def __init__(self, model, __vin, __numbers):
         self.model = model
         self.__vin = __vin
         self.__numbers = __numbers
-        self.__is_valid_vin(__vin)
-        self.__is_valid_numbers(__numbers)
+        self.__is_valid_vin(vin_number=__vin)
+        self.__is_valid_numbers(numbers=__numbers)
 
     def __is_valid_vin(self, vin_number):
-        self.__vin = vin_number
+        self.vin_number = vin_number
         if not isinstance(vin_number, int):
             raise IncorrectVinNumber('Некорректный тип vin номер')
         elif vin_number not in range(1000000, 9999999):
@@ -16,7 +48,7 @@ class Car:
             return True
 
     def __is_valid_numbers(self, numbers):
-        self.__numbers = numbers
+        self.numbers = numbers
         if not isinstance(numbers, str):
             raise IncorrectCarNumbers('Некорректный тип данных для номеров')
         elif len(numbers) != 6:
@@ -55,6 +87,24 @@ if __name__ == '__main__':
         print(exc.message)
     else:
         print(f'{second.model} успешно создан')
+
+    # try:
+    #     first = Car('Model1', 1000000, 'f123dj')
+    # except IncorrectVinNumber as exc:
+    #     print(exc.message)
+    # except IncorrectCarNumbers as exc:
+    #     print(exc.message)
+    # else:
+    #     print(f'{first.model} успешно создан')
+    #
+    # try:                                        Почему то здесь Модель1 и Модель2 повторяются?
+    #     second = Car('Model2', 300, 'т001тр')
+    # except IncorrectVinNumber as exc:
+    #     print(exc.message)
+    # except IncorrectCarNumbers as exc:
+    #     print(exc.message)
+    # else:
+    #     print(f'{second.model} успешно создан')
 
     try:
         third = Car('Model3', 2020202, 'нет номера')
